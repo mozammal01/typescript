21f0793403d2e6b2db4fa1b86f4bdec3c05d039d
mozammal01 <mozammalhaq01@gmail.com>
2025-11-16 21:34:34 +0600

    solved problem 1 and 2

--- solution.ts
+++ solution.ts
@@ -1,1 +1,28 @@
-const solution = "Here is your solution";
+// Problem 1 Solution
+const formatValue = (value: string | number | boolean) => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+  else if (typeof value === "number") {
+    return value * 10;
+  }
+  else if (typeof value === "boolean") {
+   return !value
+  }
+  else {
+    return "This is not a string, number, or boolean" ;
+  }
+};
+
+
+// Problem 2 Solution
+
+const getLength = (value: string | (number | string | boolean)[]) => {
+  if(Array.isArray(value))  {
+    return value.length;
+  }else if(typeof value === "string") {
+    return value.length;
+  }else {
+    return "This is not a string or array";
+  }
+}
